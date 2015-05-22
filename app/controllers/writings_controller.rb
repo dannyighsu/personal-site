9abc90277dad3b55e012ab20e9a9df3e60bd1c13
Danny Hsu <danny@kloudless.com>
2015-05-22 00:09:08 -0400

    Attempting to fix url spacing issue

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -2,6 +2,11 @@ class WritingsController < ApplicationController
 
   def show
     @writing = Writing.all.reverse
+    @writing.each do |w|
+      if w.url.include? " "
+        w.url.gsub!(" ", "\ ")
+      end
+    end
   end
 
 end
