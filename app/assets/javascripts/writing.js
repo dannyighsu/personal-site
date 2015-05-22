7eda8f8d255c9be6a5e4263de6f7ac50a1bfc8f7
Danny Hsu <danny@kloudless.com>
2015-05-22 00:26:44 -0400

    Changed writing partial to get id by database id

--- app/assets/javascripts/writing.js
+++ app/assets/javascripts/writing.js
@@ -1,7 +1,9 @@
 function showDocument(input) {
+    console.log(input);
+    input = document.getElementById(input);
     if (input.height != 600) {
-        input.height = 600
+        input.height = 600;
     } else {
-        input.height = 0
+        input.height = 0;
     }
 }
--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -2,11 +2,6 @@ class WritingsController < ApplicationController
 
   def show
     @writing = Writing.all.reverse
-    @writing.each do |w|
-      if w.url.include? " "
-        w.url.gsub!(" ", "\ ")
-      end
-    end
   end
 
 end
